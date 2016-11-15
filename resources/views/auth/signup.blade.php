b9aec0eef338753a71ea45e32f208121d57092ef
lacr1097 <lacr.1097@gmail.com>
2016-11-14 23:53:07 -0600

    models updated, signup view created and some visual improvements

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -7,32 +7,83 @@ Sign up on league-project
 @section('css')
 <style>
     body{
-        background-color: #589;    
+        background-color: #222525;    
     }
     .whiteCard{
-        background-color: white;
-        border: 1px solid #eee;
+        background-color: #111;
         border-radius: 3px;
+        box-shadow: 0px 1px 1px 0px #000;
+        color: white;
     }
     #signupCard{
-        margin-left: 20%;
-        margin-right: 20%;
-        margin-bottom: 30px;
+        margin-bottom: 80px;
+        padding: 0px;
+    }
+    .navBar{
+        background-color: #000;
+    }
+    .header{
+        background-color: dodgerblue;
+        box-shadow: inset 0px -2px 1px 0px #1d80dd;
+        border-top-left-radius: 3px;
+        border-top-right-radius: 3px;
+        padding: 10px;
+        width: 100%;
+        margin: 0px;
+    }
+    .header > h3{
+        margin: 0px;
+        color: white;
+    } 
+    .footer{
+        background-color: #111;
+        box-shadow: none;
+    }
+    #signupContainer{
+        padding-top:90px; 
+        background-color: transparent; 
+        width: 100%;
+        -webkit-transition: padding .6s, opacity 1s;
+        opacity: 0;
     }
 </style>
 @endsection
 
 @section('body')
-<div class="" style="padding-top:80px; background-color: transparent; width: 100%;">
-    <div class="whiteCard" id="signupCard">
-        <h3>Sign up</h3>
-        <p>d</p>
-        <p>d</p>
-        <p>ds</p>
-        <p>d</p>
+<div id="signupContainer">
+    <div class="whiteCard col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1" id="signupCard">
+        <div class="header">
+            <h3>Sign up</h3>
+        </div>
+        <div style="padding: 20px;">
+            <div class="form-group ">
+                <label style="margin-right: 10%;" for="">Name</label>
+                <input type="text" class="myInputWhite myInput-large">
+            </div>
+            <div class="form-group ">
+                <label style="margin-right: 10%;" for="">Email</label>
+                <input type="email" class="myInputWhite myInput-large">
+            </div>
+            <div class="form-group ">
+                <label style="margin-right: 10%;" for="">Password</label>
+                <input type="password" class="myInputWhite myInput-large">
+            </div>
+            <div class="form-group ">
+                <label style="margin-right: 10%;" for="">Password confirm</label>
+                <input type="password" class="myInputWhite myInput-large">
+            </div>
+            <div class="form-group" style="margin-bottom: 0px; margin-top: 20px;">
+                <button class="btn btnBlue" style="width:100%;">Sign up!</button>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
 
 @section('js')
+<script>
+    $(function($){
+        $('#signupContainer').css('padding-top','130px').css('opacity','1');
+    });
+</script>
 @endsection
