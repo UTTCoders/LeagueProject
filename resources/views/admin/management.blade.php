f59509a37a9589f8ed65a04067159ba40cbaa9ca
lacr1097 <lacr.1097@gmail.com>
2016-11-16 18:02:48 -0600

    Manage view updated
    Sorry for dont do it on another branch, I forget it!
    It is not gonna happen again

--- resources/views/admin/management.blade.php
+++ resources/views/admin/management.blade.php
@@ -6,18 +6,51 @@ League management
 
 @section('css')
 <style>
+    body{
+      background:  #222525;
+    }
     .coverContainer{
         height: 400px;
         background-image: url("{{asset('img/management.jpg')}}");
         box-shadow: inset 0px -2px 8px 0px #222;
     }
     .mainTitle{
-        text-align: center; 
-        margin: 0; 
+        text-align: center;
+        margin: 0;
         top: 180px;
         color: white;
         text-shadow: 0px 0px 2px #000;
         position: relative;
+    }
+    #manageMenu{
+        margin-top: 50px;
+        position: relative;
+        border: 1px solid rgba(0,0,0,.4);
+        border-radius: 3px;
+        padding: 0;
+        overflow: hidden;
+        box-shadow: 0px 1px 1px 0px rgba(0,0,0,.2);
+    }
+    .manageMenuHeader{
+        background-color: dodgerblue;
+        border-top: 1px solid skyblue;
+        position: relative;
+        text-align: center;
+        margin: 0px;
+        padding: 3px;
+    }
+    .manageMenuItem{
+        background-color: rgba(0,0,0,.1);
+        position: relative;
+        border: 1px solid transparent;
+        text-align: center;
+        margin: 0px;
+        padding: 3px;
+        -webkit-transition: background-color .1s;
+    }
+    .manageMenuItem:hover{
+        border: 1px solid #333;
+        background-color: rgba(255,255,255,.01);
     }
 </style>
 @endsection
@@ -25,6 +58,17 @@ League management
 @section('body')
 <div class="coverContainer">
     <h2 class="mainTitle">Add teams, stadiums, players, coaches and more</h2>
+</div>
+<div class="col-md-12">
+    <div class="col-md-6">
+
+    </div>
+    <div class="col-md-2 col-md-offset-2" id="manageMenu">
+      <a class="manageMenuHeader col-md-12">Manage...</a>
+      <a href="#" class="manageMenuItem col-md-12">Stadiums</a>
+      <a href="#" class="manageMenuItem col-md-12">Coaches</a>
+        <a href="#" class="manageMenuItem col-md-12">Teams</a>
+    </div>
 </div>
 @endsection
 
