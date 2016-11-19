7c3ff1282a86a6e284879de478d68577bc0bd89c
Isra de Leon <isra-n@outlook.com>
2016-11-19 07:55:14 -0600

    I just added the map to the user view, I still need to learn how the google maps api works

--- resources/views/user/userhome.blade.php
+++ resources/views/user/userhome.blade.php
@@ -4,13 +4,25 @@
 
 @section('css')
 <link rel="stylesheet" type="text/css" href="/css/someanyicss.css">
+<style type="text/css"></style>
 @endsection
 
 @section('body')
 <div class="coverContainer">
-    <h2 id="mainTitle">Get in news about your favorite teams, follow their matches in real time,<br>view statistics and more...</h2>
+    <h2 style="color:white; margin-top: 100px;" id="mainTitle">Get in news about your favorite teams, follow their matches in real time,<br>view statistics and more...</h2>
 </div>
-<div class="loginMargin">
+<div id="map"></div>
+@endsection
 
-</div>
+@section('js')
+<script>
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 0, lng: 0},
+        zoom: 6
+    });
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAqiB2cyhlFaZJmw6_x1Cz7-AvGH5dkTLU&callback=initMap&language=EN" async defer></script>
 @endsection
