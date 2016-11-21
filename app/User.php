b3b93a512363cf9b687d0b69fbd50d12811b9cf9
Isra de Leon <isra-n@outlook.com>
2016-11-21 15:31:48 -0600

    A manual merge in user.php

--- app/User.php
+++ app/User.php
@@ -9,22 +9,16 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-<<<<<<< HEAD
-=======
     public $timestamps=false;
 
->>>>>>> 5656ef3b7aa1c4f042c884ce4c8ecfca020537e5
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-<<<<<<< HEAD
-        'name', 'email', 'password',
-=======
+
         'name', 'email', 'password', 'type', 'active'
->>>>>>> 5656ef3b7aa1c4f042c884ce4c8ecfca020537e5
     ];
 
     /**
@@ -35,8 +29,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-<<<<<<< HEAD
-=======
+
 
     public function teams(){
         return $this->belongsToMany('App\League\Team');
@@ -45,5 +38,4 @@ class User extends Authenticatable
     public function comments(){
         return $this->belongsToMany('App\League\Match')->withPivot('content','date');
     }
->>>>>>> 5656ef3b7aa1c4f042c884ce4c8ecfca020537e5
 }
