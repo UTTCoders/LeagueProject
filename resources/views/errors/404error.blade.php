3929c836880cfff33ce42e1fab76ef765edc8f21
Isra de Leon <isra-n@outlook.com>
2016-11-22 18:02:49 -0600

    More changes to stadium view

--- resources/views/errors/404error.blade.php
+++ resources/views/errors/404error.blade.php
@@ -16,7 +16,8 @@
 
 @section('body2')
 <div class="container" id="error" align="center">
-	<h1>:'( 404</h1>
+	<h1 style="font-size: 70px;">:'(</h1>
+	<h1>404</h1>
 	<h2>Page not found dude!</h2>
 </div>
 @endsection
--- resources/views/user/stadiumview.blade.php
+++ resources/views/user/stadiumview.blade.php
@@ -3,9 +3,18 @@
 @section('css2')
 <style type="text/css">
 	#con{
-		background-color: #388E3C;
+		background-color: #000;
 		width: 100%;
 		margin-top: 62px;/*size of the navbar*/
+		background-image: url(/storage/{{$stadium->photo}});
+		background-repeat: no-repeat;
+		background-size: 100%;
+		background-position: center;
+		margin-bottom: 0;
+		margin-top: 0;
+		border-bottom: 0;
+		padding-top:100px;
+		padding-bottom: 100px;
 	}
 	body{
 		background-color: white;
@@ -15,7 +24,7 @@
 
 @section('body2')
 <div id="con">
-	
+
 </div>
 <div class="">
 	
