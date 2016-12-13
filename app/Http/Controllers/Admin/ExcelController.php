82e28759bd1c92432ce6ca2a2e58ee80c8272ee3
Isra de Leon <isra-n@outlook.com>
2016-12-13 17:57:54 -0600

    Working with the excel issue

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -25,6 +25,35 @@ class ExcelController extends Controller
     		$r->file('excelFile')->getClientOriginalExtension() != 'ots') {
     		return back()->with('msg.content',"The file format is invalid!");
     	}
+    	$m=array();
+    	Excel::load($r->file('excelFile'), function($reader){
+    		$reader->each(function($sheet){
+    			if ($sheet->getTitle() === "players") {
+                    $sheet->each(function($row){
+                    	$theteam=Team::where('name',$row["team"])->first();
+                    	if ($theteam) {
+                    		$theplayer=Player::where("shirt_number",
+                    		$row["shirt_number"])->first();
+                    		if (!$theplayer) {
+		                        $p=new Player;
+		                        $p->name=$row["name"];
+		                        $p->last_name=$row["last_name"];
+		                        $p->team_id=$theteam->id;
+		                        $p->shirt_number=$row["shirt_number"];
+		                        $p->nationality=$row["nationality"];
+		                        $p->save();
+                    		}else{
+                    			$m[]="cucu";
+                    		}
+                    	}else{
+                    		$m[]="cucu2";
+                    	}
+                    });
+                }
+    		});
+    	});
+
+    	return $m;
     	$excel=Excel::load($r->file('excelFile'))->get();
     	return $excel;
     	return $r->file('excelFile')->getClientOriginalExtension();
--- config/app.php
+++ config/app.php
@@ -227,7 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class
     ],
 
 ];
