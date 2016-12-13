b5c43487b97a5a300de83ca6fc928ce0783d3822
Isra de Leon <isra-n@outlook.com>
2016-12-13 10:42:31 -0600

    DElete the hardcore

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\League\Team;
+use App\League\Team;
 
 class ExcelController extends Controller
 {
 	public function PlayersRequest(Request $r){
-		
+
 	}
 
     public function PlayersAdd(Request $r){
