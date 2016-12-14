5aa5cf6775ed0ea4f63363eda1ceb799edeef665
Isra de Leon <isra-n@outlook.com>
2016-12-13 19:19:51 -0600

    Int issue again

--- resources/views/user/events.blade.php
+++ resources/views/user/events.blade.php
@@ -1,6 +1,6 @@
 @if($match->events()->count() > 0)
 	@foreach($match->events as $e)
-		<div style="color:#eee; background-color: #444;" align="center" class="thumbnail event"><span class="pull-left"><img style="width:16px;" src="/{{$e->eventType->icon}}"></span>{{$e->content}}<span class="pull-right">Min: {{date_format(date_create((int)$e->minute),"i")}}</span></div>
+		<div style="color:#eee; background-color: #444;" align="center" class="thumbnail event"><span class="pull-left"><img style="width:16px;" src="/{{$e->eventType->icon}}"></span>{{$e->content}}<span class="pull-right">Min: {{$e->minute}}</span></div>
 	@endforeach
 @else
 	<br>
